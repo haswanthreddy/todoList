88ebd4c8c0513faae5e3bc7341ae2148583b6e86
haswanthreddy <v.haswanthreddy@gmail.com>
2022-11-20 20:22:16 +0530

    implemented social login passport and cookie session

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,4 +1,5 @@
-require('dotenv').config()
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const {
 	ATLAS_USER,
@@ -6,7 +7,11 @@ const {
 	ATLAS_CLUSTER,
 	MONGO_DB,
 	PORT = 3000,
-	CLIENT
+	CLIENT,
+	CLIENT_ID,
+	CLIENT_SECRET,
+	SECRET_KEY_1,
+	SECRET_KEY_2,
 } = process.env;
 
 const mongoConnectionString = `mongodb+srv://${ATLAS_USER}:${ATLAS_PASSWORD}@${ATLAS_CLUSTER}/${MONGO_DB}?retryWrites=true`;
@@ -18,5 +23,9 @@ module.exports = {
 	MONGO_DB,
 	PORT,
 	CLIENT,
-	mongoConnectionString
-}
+	mongoConnectionString,
+	CLIENT_ID,
+	CLIENT_SECRET,
+	SECRET_KEY_1,
+	SECRET_KEY_2,
+};
